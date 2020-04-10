c92758b5d79719e74c6b69f4ad6f2669d396681d
jmariam <jmariam@ceiamerica.com>
2020-04-10 15:15:02 +0530

    Exception Program

--- Sample/Sample/Program.cs
+++ Sample/Sample/Program.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using System;using System.Collections.Generic;
 
-namespace Sample
-{
-    class Program
+namespace Sample{
+
+    class GenericClass
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            try
+            {
+                int a = 10;
+                int b = 0;
+                int x = a / b;
+            }
+            
+
+            catch (NullReferenceException e) { Console.WriteLine(e); }
+            finally { Console.WriteLine("Finally block is executed"); }
+            Console.WriteLine("Rest of the code");
         }
     }
 }
