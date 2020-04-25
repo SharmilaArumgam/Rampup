7c089a92098d8c0822a6fe547a7a86e3a4a9f317
jmariam <jmariam@ceiamerica.com>
2020-04-25 13:36:46 +0530

    MVC Initial commit

---
+++ Mvc_Start/Controllers/MusicStoreController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_Start.Controllers
+{
+    public class MusicStoreController : Controller
+    {
+        // GET: MusicStore
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public string Welcome()
+        {
+            return "Hello, this is welcome action message";
+        }
+
+        public string ShowMusic(string MusicTitle)
+        {
+            return "Sharmila selected " + MusicTitle + " Music";
+        }
+    }
+}
